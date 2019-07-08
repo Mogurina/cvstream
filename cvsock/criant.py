b65b7b7500a2aac84319d9f4ce881b7ab29f8406
mogurina <mogumogutyannneru@icloud.com>
2019-07-08 21:36:03 +0900

    change

--- cvsock/criant.py
+++ cvsock/criant.py
@@ -4,13 +4,14 @@ import sock_func_for_cv as scv
 import numpy as np
 import time
 def main():
-    s = scv.connect("172.16.10.104",55001)
+    s = scv.connect("192.168.0.31",55001)
     with s:
         #cv2.namedWindow("criant", cv2.WINDOW_NORMAL)
         while(cv2.waitKey() != "q"):
             img = scv.recvimg(s)
             if img is False:
                 break
+            print("image data:",img.shape)
             cv2.imshow("criant",img)
     print("終了")
     cv2.destroyAllWindows()
--- cvsock/server.py
+++ cvsock/server.py
@@ -3,12 +3,17 @@ import socket
 import sock_func_for_cv as scv
 import time
 def main():
-    cap = cv2.VideoCapture(0)
-    with scv.socket_set_up("172.16.10.104",55001) as sock:
+    frame = scv.getimg()
+    #print("webカメラのセットアップ中")
+    #cap = cv2.VideoCapture(0)
+    #if cap is None:
+    #    ("セットアップ失敗")
+    #print("セットアップ完了")
+    with scv.socket_set_up("192.168.0.31",55001) as sock:
         #cv2.namedWindow("server", cv2.WINDOW_NORMAL)
-        #img = scv.getimg()
         while(cv2.waitKey() != "q"):
-            flag,frame = cap.read()
+            #flag,frame = cap.read()
+            print("image data:",frame.shape)
             scv.sendimg(sock,frame)
             cv2.imshow("server",frame)
             
